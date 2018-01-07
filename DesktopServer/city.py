0e78df1b96c90492a3979d320bfe249d7952a354
alex4men <ezh25pr@hotmail.com>
2018-01-07 13:44:58 +0300

    Added Buttons

--- DesktopServer/city.py
+++ DesktopServer/city.py
@@ -6,22 +6,47 @@ except ImportError: # For Python3
 import time
 
 class App():
-
     def __init__(self):
-        self.root = tk.Tk() # Main window
+        self.defLabel = "00:00.00"
 
-        self.label = tk.Label(self.root, text="", font=('DSEG7Classic-Italic', 40), bg='black', 
+        self.root = tk.Tk() # Main window
+        self.root.title("Robotraffic city timer")
+        self.frame = tk.Frame(self.root) # TODO: Add black background
+        self.frame.pack(fill='x')
+
+        self.label = tk.Label(self.frame, text=self.defLabel, font=('DSEG7Classic-Italic', 240), 
          fg='blue')
         self.label.pack(fill='x')
-        self.update_clock()
-        self.root.mainloop()
 
-    def __exit__(self):
-        self.root.destroy() # TODO: properly handle exit
+        self.startButton = tk.Button(self.frame, text='START', command=self.start)
+        self.startButton.pack(side='left')
+
+        self.stopButton = tk.Button(self.frame, text='STOP', command=self.stop)
+        self.stopButton.pack(side='left')
+
+        self.resetButton = tk.Button(self.frame, text='RESET', command=self.reset)
+        self.resetButton.pack(side='left')
+
+        self.isStarted = 0
+
+    def start(self):
+        self.isStarted = 1
+        self.update_clock()
+
+    def stop(self):
+        self.isStarted = 0
+
+    def reset(self):
+        self.isStarted = 0
+        self.label.configure(text=self.defLabel)
 
     def update_clock(self):
-        now = time.strftime("%H:%M:%S")
-        self.label.configure(text=now)
-        self.root.after(10, self.update_clock)
+        if self.isStarted:
+            ms = int((time.time() % 1) * 100)
+            now = time.strftime("%M:%S.") + str(ms) if ms > 9 else time.strftime("%M:%S.0") + str(ms) # Fix jiggling
+            self.label.configure(text=now)
+            self.root.after(10, self.update_clock)
+            print(time.time())
 
 app=App()
+app.root.mainloop()
