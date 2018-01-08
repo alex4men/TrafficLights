15907c575fa5d9f2280336c69615f69b6c7266c2
alex4men <ezh25pr@hotmail.com>
2018-01-08 17:04:03 +0300

    Working network

---
+++ DesktopServer/network.py
@@ -0,0 +1,55 @@
+# Use only with Python 3
+from socketserver import UDPServer, BaseRequestHandler, ThreadingMixIn
+
+import socket
+import threading
+import ipaddress
+
+class ThreadedUDPHandler(BaseRequestHandler):
+    def handle(self):
+        data = str(self.request[0][:100], 'ascii')
+        socket = self.request[1]
+        print("[Server]: {}:{} wrote: {}".format(self.client_address[0], self.client_address[1], data))
+        
+        # cur_thread = threading.current_thread()
+        # response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
+        # socket.sendto(response.upper(), self.client_address)
+
+class ThreadedUDPServer(ThreadingMixIn, UDPServer):
+    pass
+
+def client(ip, port, message):
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        # sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # delete
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        print("[Client]: Sending: {}".format(message))
+        sock.sendto(bytes(message, 'ascii'), (ip, port))
+
+        # response = str(sock.recv(1024), 'ascii')
+        # print("[Client]: Received: {}".format(response))
+
+def test():
+    print("test")
+
+if __name__ == "__main__":
+    PORT = 4210
+    HOST = ''
+    # the public network interface
+    localIP = socket.gethostbyname(socket.gethostname())
+    server = ThreadedUDPServer((HOST, PORT), ThreadedUDPHandler)
+
+    serverIP, port = server.server_address
+    broadcastIP = str(ipaddress.ip_interface(localIP+'/24').network[-1])
+
+    # Start a thread with the server -- that thread will then start one
+    # more thread for each request
+    server_thread = threading.Thread(target=server.serve_forever)
+    # Exit the server thread when the main thread terminates
+    server_thread.daemon = True
+    server_thread.start()
+    print("Server loop running in thread:", server_thread.name, serverIP, port)
+
+    
+    client(broadcastIP, port, "S")
+    # client(ip, port, "Hello World 2")
+    # client(ip, port, "Hello World 3")
